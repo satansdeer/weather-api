a25a1110c009a65f0562439c29273a6e30d53a4a
Maksim Ivanov <maxim.ivanov@toptal.com>
2018-06-13 08:27:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", function(req, res, next) {
+  axios
+    .get(
+      "https://api.darksky.net/forecast/ac579da85ad0806cb406403e638ea8a9/37.8267,-122.4233"
+    )
+    .then(resp => res.send(resp.data))
+    .catch(next);
+});
+
+app.listen(3001, () => console.log("Example app listening on port 3001!"));
